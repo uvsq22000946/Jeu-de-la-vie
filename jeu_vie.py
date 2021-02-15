9d91fc21370d7d9b303f2a6abce0a0eb86a07ef2
Maxime EBRAN <maxime.ebran@ens.uvsq.fr>
2021-02-15 09:22:27 +0100

    15-02-21

--- jeu_vie.py
+++ jeu_vie.py
@@ -14,16 +14,38 @@ import tkinter as tk
 COULEUR_FONT = "black"
 WIDTH = 500
 HEIGHT = 500
+tableau = None
+NB_COL = 20
+NB_LINE =20
+COTE = 25
 
 #################################
 # Fonctions
 
 def quadrillage():
     """Affiche un quadrillage sur le canvas"""
-    for x in range(20):
-        canvas.create_line((x * 25, 0), (x * 25, 500), fill="white")
-    for y in range(20):
-        canvas.create_line((0, y * 25), (500, y * 25), fill="white")
+    for x in range(NB_COL):
+        canvas.create_line((x * COTE, 0), (x * COTE, HEIGHT), fill="white")
+    for y in range(NB_LINE):
+        canvas.create_line((0, y * COTE), (WIDTH, y * COTE), fill="white")
+
+def conversion(x, y):
+    """Fonction qui retourne la colone et la ligne du quadrillage en fonction des coordonnés"""
+    return x // COTE, y // COTE
+
+def change_carre(event):
+    """Au clic, change l'état du carre"""
+    i, j = conversion(event.x, event.y)
+    if tableau[i][j] == -1:
+        x, y = i * COTE, j * COTE
+        canvas.create_rectangle(x, y, x + COTE, y + COTE, fill="green", outline="white")
+
+def creer_tableau():
+    """Initialise un tableau qui vaut -1 pour une case morte et l'identifiant du carre si une case est vivante"""
+    global tableau
+    tableau_col = [-1] * NB_LINE
+    tableau = [tableau_col for i in range(NB_COL)]
+
 
 #################################
 # Programme principal
@@ -34,7 +56,14 @@ racine.title("Jeu de la vie")
 #Creation des widgets
 canvas = tk.Canvas(racine, width=WIDTH, height=HEIGHT, bg=COULEUR_FONT)
 quadrillage()
+creer_tableau()
 
 # Positionnent des widgets
 canvas.grid()
+
+# Traitement des events
+
+canvas.bind('<Button-1>', change_carre)
+
+
 racine.mainloop()
